018c9b1c6536c474c6a89e07c28e75334b2207fc
artem.kushnir <artem.kushnir@strana.com>
2025-07-19 11:34:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MigrationService;
+using MigrationService.BLL;
+using MigrationService.DB.Adapters;
+using MigrationService.DB.Contexts;
+using Serilog;
+using System.Globalization;
+using System.Reflection;
+
+
+var host = Host.CreateDefaultBuilder()
+            .ConfigureHostConfiguration(configuration =>
+            {
+                configuration.SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                configuration.AddJsonFile("appsettings.json", true, true);
+                configuration.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("Environment")}.json", true, true);
+                configuration.AddEnvironmentVariables();
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.Configure<AppOptions>(context.Configuration);
+                services.AddSingleton<App>();
+                services.AddTransient<MigrationProvider>();
+                services.AddLogging(builder => builder.AddSerilog());
+
+                var adapter = context.Configuration.GetValue<string>("DBProvider");
+                switch (adapter)
+                {
+                    case "Oracle":
+                        services.AddTransient<IDBContext, OracleContext>();
+                        services.AddTransient<IDBAdapter, OracleAdapter>();
+                        break;
+                    default:
+                        break;
+                }
+            })
+            .Build();
+
+var app = host.Services.GetService<App>();
+app?.Run();
